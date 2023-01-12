de5b57738447887a6b551d10c216b3737e851ef1
Milan Bhandari <108926680+milan-says@users.noreply.github.com>
2023-01-12 15:23:47 +0545

    Update Request.tsx

--- src/Request.tsx
+++ src/Request.tsx
@@ -3,6 +3,7 @@ const rootUrl="https://api.themoviedb.org";
 
 const requests = {
   requestPopular: `${rootUrl}/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestRomantic: `${rootUrl}/3/movie/popular?api_key=${key}&language=en-US&page=1`,
   requestTopRated: `${rootUrl}/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
   requestTrending: `${rootUrl}/3/movie/popular?api_key=${key}&language=en-US&page=2`,
   requestHorror: `${rootUrl}/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
